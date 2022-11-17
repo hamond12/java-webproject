ffa8ba79b9880396029ba447dd15d9585f5a67f6
songjuyoung-1410 <chicken010208@gmail.com>
2022-11-17 12:11:57 +0900

    asdf

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,80 +1,18 @@
 import MHeader from "../components/Main/MHeader";
 import styled from "styled-components";
+import Write from "../components/Write/Write";
 
 const Wdiv = styled.div`
   margin-top: 75px;
 `;
 
-const Wtitle = styled.div`
-  font-size: 50px;
-  font-weight: 500;
-  margin-left: 120px;
-  margin-top: 100px;
-  padding: 10px;
-`;
-
-const Wt = styled.p`
-  margin-left: 150px;
-  margin-top: 30px;
-  font-size: 20px;
-`;
-
-const Wtb = styled.input`
-  margin-left: 150px;
-  font-size: 17px;
-  padding: 7px;
-  text-indent: 5px;
-  border-radius: 5px;
-  width: 75%;
-`;
-
-const Textbox = styled.textarea`
-  width: 75%;
-  margin-top: 2px;
-  margin-left: 150px;
-  border-radius: 10px;
-  height: 200px;
-  font-size: 17px;
-  padding-left: 10px;
-  padding-top: 10px;
-`;
-
-const Btndiv = styled.div`
-  width: 100%;
-  text-align: center;
-  margin-left: -20px;
-  margin-bottom: 40px;
-`;
-
-const Goodbtn = styled.button`
-  margin-top: 25px;
-  border: none;
-  border-radius: 5px;
-  width: 130px;
-  height: 40px;
-  line-height: 40px;
-  background-color: #3fcdfa;
-  color: white;
-  font-size: 18px;
-  text-align: center;
-`;
-
-export default function WirtePage() {
+export default function WritePage() {
   return (
     <>
       <MHeader />
       <Wdiv>
-        <Wtitle>글쓰기</Wtitle>
-        <Wt>제목</Wt>
-        <Wtb type="text" placeholder="제목을 입력하세요."></Wtb>
-        <Wt>작성자</Wt>
-        <Wtb type="text" placeholder="작성자명을 입력하세요."></Wtb>
-        <Wt>내용</Wt>
-        <Textbox placeholder="내용을 입력하세요."></Textbox>
+        <Write />
       </Wdiv>
-      <Btndiv>
-        <Goodbtn>글쓰기 완료</Goodbtn>
-      </Btndiv>
     </>
   );
 }
